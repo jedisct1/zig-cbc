d7770742b36404e0ae1f0167ab64c948abbbc1ad
Frank Denis <github@pureftpd.org>
2023-05-08 20:55:04 +0200

    Partially revert 025b3446d9ed784dcc58a883a6e16a88a37ba6b9
    PKCS7 actually requires padding if the last block is empty.

--- src/main.zig
+++ src/main.zig
@@ -28,7 +28,7 @@ pub fn CBC(comptime BlockCipher: anytype) type {
 
         /// Return the length of the ciphertext given the length of the plaintext.
         pub fn paddedLength(length: usize) usize {
-            return (std.math.divCeil(usize, length, EncryptCtx.block_length) catch unreachable) * EncryptCtx.block_length;
+            return (std.math.divCeil(usize, length + 1, EncryptCtx.block_length) catch unreachable) * EncryptCtx.block_length;
         }
 
         /// Encrypt the given plaintext for the given IV.
@@ -48,15 +48,13 @@ pub fn CBC(comptime BlockCipher: anytype) type {
                 @memcpy(dst[i..][0..block_length], &cv);
             }
             // Last block
-            if (i < src.len) {
-                var in = [_]u8{0} ** block_length;
-                var padding_length = @intCast(u8, padded_length - src.len);
-                @memset(in[padding_length..], padding_length);
-                @memcpy(in[0 .. src.len - i], src[i..]);
-                for (cv[0..], in) |*x, y| x.* ^= y;
-                self.enc_ctx.encrypt(&cv, &cv);
-                @memcpy(dst[i..], cv[0 .. dst.len - i]);
-            }
+            var in = [_]u8{0} ** block_length;
+            var padding_length = @intCast(u8, padded_length - src.len);
+            @memset(&in, padding_length);
+            @memcpy(in[0 .. src.len - i], src[i..]);
+            for (cv[0..], in) |*x, y| x.* ^= y;
+            self.enc_ctx.encrypt(&cv, &cv);
+            @memcpy(dst[i..], cv[0 .. dst.len - i]);
         }
 
         /// Decrypt the given ciphertext for the given IV.
@@ -97,7 +95,7 @@ test "CBC mode" {
     const key = [_]u8{ 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
     const iv = [_]u8{ 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
     const src_ = "This is a test of AES-CBC that goes on longer than a couple blocks. It is a somewhat long test case to type out!";
-    const expected = [_]u8{ 232, 208, 30, 75, 213, 218, 29, 122, 173, 231, 214, 236, 102, 221, 80, 142, 186, 36, 76, 222, 65, 239, 134, 19, 224, 174, 219, 250, 65, 254, 229, 176 };
+    const expected = [_]u8{ 32, 252, 200, 238, 32, 28, 17, 125, 123, 151, 155, 190, 31, 191, 253, 139, 24, 221, 222, 242, 8, 19, 219, 65, 255, 8, 14, 245, 190, 73, 192, 187 };
 
     try comptime std.testing.expect(src_.len / M.paddedLength(1) >= 3); // Ensure that we have at least 3 blocks
 
